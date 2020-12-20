9a9da9efdc799c878594e2ced2c3c847e35c9adf
CharliB <barros.rcarlos@gmail.com>
2020-12-20 13:02:23 -0300

    DB conection

---
+++ clases/conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+    class Conexion
+    {
+        private function __construct()
+        {} // evitamos que se instancie
+
+        static function conectar()
+        {
+            $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+            $link = new PDO(
+                'mysql:host=localhost;dbname=spicycakes',
+                'root',
+                '',
+                $opciones
+            );
+            $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            return $link;
+        }
+    }
